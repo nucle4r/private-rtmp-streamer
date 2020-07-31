4065b8fe702ec9f1091a4c1336bece820d2aee7e
Priyanshu Singh <35601449+nucle4r@users.noreply.github.com>
2020-07-31 21:04:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const spawn = require('child_process').spawn;
 
-let FilePath = 'https://r4---sn-npoeened.googlevideo.com/videoplayback?expire=1596230244&ei=BDYkX5PBMoaBwgTIv47ABQ&ip=45.235.100.30&id=o-AKqjbLTln30ggnciNlGk8xgXuUI53d6mLIc2VSxNzt5W&itag=22&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=5861.227&lmt=1566896041396528&fvip=4&fexp=23883098&c=WEB&txp=2216222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgI2VkDMoTmrUcoMZxPfFrACSsYY19odl4zmdsIemhxsgCIQC_leQtPgtfXc-M0ahaH3dc3XTsU8VNV4IdWRa4t4Z16Q%3D%3D&contentlength=1310984199&video_id=f5SOWctSAO0&title=Live+ASMR+%E2%99%A5+use+Headphones+%E2%99%A5+NEW+Triggers+-+Amouranth+Full+Stream+8-26&rm=sn-jucj-0qps7s,sn-bg0kl7z&req_id=5236f5c62b75a3ee&cm2rm=sn-bvvbax-jj0e7z,sn-h55e67z&ipbypass=yes&redirect_counter=4&cms_redirect=yes&mh=ug&mip=117.197.70.81&mm=34&mn=sn-npoeened&ms=ltu&mt=1596208581&mv=m&mvi=4&pl=20&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgS1nBUKWk3PQoVvEitsY0O3kqprVUT6ZpUdB2hCUce5kCIQCqRdxsG6bDQJd9EyIB2HgqKx3gUoV770ambdWu1eFHqw%3D%3D';  // Your Video File you want To loop through.
+let FilePath = 'https://r3---sn-i3b7kn7d.googlevideo.com/videoplayback?expire=1596222479&ei=rhckX-WbOpu1lQTb97j4Dg&ip=115.88.201.46&id=o-AORg_Ep9VgiYp35zJ2vq63Q9BWdxTsvj6O6d9MbDYb0F&itag=22&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=979.208&lmt=1542542671095604&fvip=3&fexp=23883098&c=WEB&txp=2201222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgU9j30PpY8LThXikWIfXgYFgssjaLi6Z-vMQFzq4L0aUCIQCq2g18EUC2ygxRWKUFlvfc54Xoqj4FbFJwHiUrNSzvew%3D%3D&contentlength=304503446&video_id=aL5oRsZGI4o&title=Pubg+Mobile+HD+Gameplay++-+No+Copyright+Gamepaly+%2860+FPS%29&rm=sn-ab02a0nfpgxapox-bh2lk7y,sn-ab02a0nfpgxapox-jwwe7z&req_id=257dd4d82bfba3ee&ipbypass=yes&redirect_counter=2&cms_redirect=yes&mh=NH&mm=30&mn=sn-i3b7kn7d&ms=nxu&mt=1596209534&mv=m&mvi=3&pl=18&lsparams=ipbypass,mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhALirwt8rJPkdDCNSLSDY5qxnvVjOxGAIVu2Cv-ZHyD28AiEAw-jO2NYNQctY6eHNXxQa7VzMCjPR8WBrnIRJyGtxEsw%3D';  // Your Video File you want To loop through.
 
-let streamKey = '5WwPjB';
+let streamKey = 'XAgbjK';
 
 //You Can Change the RTMP url as per your need. For Ex: YouTube, Twitch etc.
  
