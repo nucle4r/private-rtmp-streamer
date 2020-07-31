b4ebe7a46da8c8c600b815fe56b16b177ec18611
Priyanshu Singh <35601449+nucle4r@users.noreply.github.com>
2020-07-31 21:21:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const spawn = require('child_process').spawn;
 
 let FilePath = 'https://r3---sn-i3b7kn7d.googlevideo.com/videoplayback?expire=1596222479&ei=rhckX-WbOpu1lQTb97j4Dg&ip=115.88.201.46&id=o-AORg_Ep9VgiYp35zJ2vq63Q9BWdxTsvj6O6d9MbDYb0F&itag=22&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=979.208&lmt=1542542671095604&fvip=3&fexp=23883098&c=WEB&txp=2201222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgU9j30PpY8LThXikWIfXgYFgssjaLi6Z-vMQFzq4L0aUCIQCq2g18EUC2ygxRWKUFlvfc54Xoqj4FbFJwHiUrNSzvew%3D%3D&contentlength=304503446&video_id=aL5oRsZGI4o&title=Pubg+Mobile+HD+Gameplay++-+No+Copyright+Gamepaly+%2860+FPS%29&rm=sn-ab02a0nfpgxapox-bh2lk7y,sn-ab02a0nfpgxapox-jwwe7z&req_id=257dd4d82bfba3ee&ipbypass=yes&redirect_counter=2&cms_redirect=yes&mh=NH&mm=30&mn=sn-i3b7kn7d&ms=nxu&mt=1596209534&mv=m&mvi=3&pl=18&lsparams=ipbypass,mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhALirwt8rJPkdDCNSLSDY5qxnvVjOxGAIVu2Cv-ZHyD28AiEAw-jO2NYNQctY6eHNXxQa7VzMCjPR8WBrnIRJyGtxEsw%3D';  // Your Video File you want To loop through.
 
-let streamKey = 'EOowMT';
+let streamKey = 's3sAQx';
 
 //You Can Change the RTMP url as per your need. For Ex: YouTube, Twitch etc.
  
