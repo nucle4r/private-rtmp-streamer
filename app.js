0eca4a549f4a4113db19bf4df12c6b0c565ebdd0
Priyanshu Singh <35601449+nucle4r@users.noreply.github.com>
2020-07-31 21:28:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const spawn = require('child_process').spawn;
 
-let FilePath = 'https://r3---sn-i3b7kn7d.googlevideo.com/videoplayback?expire=1596222479&ei=rhckX-WbOpu1lQTb97j4Dg&ip=115.88.201.46&id=o-AORg_Ep9VgiYp35zJ2vq63Q9BWdxTsvj6O6d9MbDYb0F&itag=22&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=979.208&lmt=1542542671095604&fvip=3&fexp=23883098&c=WEB&txp=2201222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgU9j30PpY8LThXikWIfXgYFgssjaLi6Z-vMQFzq4L0aUCIQCq2g18EUC2ygxRWKUFlvfc54Xoqj4FbFJwHiUrNSzvew%3D%3D&contentlength=304503446&video_id=aL5oRsZGI4o&title=Pubg+Mobile+HD+Gameplay++-+No+Copyright+Gamepaly+%2860+FPS%29&rm=sn-ab02a0nfpgxapox-bh2lk7y,sn-ab02a0nfpgxapox-jwwe7z&req_id=257dd4d82bfba3ee&ipbypass=yes&redirect_counter=2&cms_redirect=yes&mh=NH&mm=30&mn=sn-i3b7kn7d&ms=nxu&mt=1596209534&mv=m&mvi=3&pl=18&lsparams=ipbypass,mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhALirwt8rJPkdDCNSLSDY5qxnvVjOxGAIVu2Cv-ZHyD28AiEAw-jO2NYNQctY6eHNXxQa7VzMCjPR8WBrnIRJyGtxEsw%3D';  // Your Video File you want To loop through.
+let FilePath = 'https://r3---sn-cvh76nez.googlevideo.com/videoplayback?expire=1596229950&ei=3jQkX5v9LZbxigSh1LvgBg&ip=206.198.131.172&id=o-AEfDBT80nIfsD6HPcXEbOqXE-lR2ea5wNKd41jB_B-9h&itag=22&source=youtube&requiressl=yes&pcm2=yes&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=7707.074&lmt=1567517500571737&fvip=3&fexp=23883098&c=WEB&txp=2216222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cpcm2%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAJGBqQXohyjFS5HKM1S6p9Oucsob5T253KoST_7KrkJwAiEAgpFdQH7OUcaRTXXQ1JWMskPb0vKuUJ4AhH-b8MmDwQ4%3D&contentlength=1406944529&video_id=iBL5zGp5ynU&title=Live+ASMR+Amouranth+Stream+%E2%99%A5+use+headphones+%E2%99%A5+RELAXING&redirect_counter=1&rm=sn-5uakd7s&req_id=7cfafb27ba65a3ee&cms_redirect=yes&ipbypass=yes&mh=pE&mip=117.197.70.81&mm=31&mn=sn-cvh76nez&ms=au&mt=1596210835&mv=u&mvi=3&pl=16&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAJn66sOJ16JYURsKwn301CADgokFERfBBRxT2rI66E5gAiAyVvObqVtTGHwJlk_9IzHSjCYH3Avzl6fwn9ZekRzqJQ%3D%3D';  // Your Video File you want To loop through.
 
-let streamKey = 's3sAQx';
+let streamKey = 'bMKcHJ';
 
 //You Can Change the RTMP url as per your need. For Ex: YouTube, Twitch etc.
  
